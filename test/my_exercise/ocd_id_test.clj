359937478c9d91edcd61c3d72db8933cd4fafb26
Anonymous Applicant <anonymous@example.com>
2017-12-14 11:38:50 -0700

    Create tests for building ocd-id's

---
+++ test/my_exercise/ocd_id_test.clj
@@ -0,0 +1,54 @@
+(ns my-exercise.ocd-id-test
+  (:require [clojure.test :refer :all]
+            [my-exercise.ocd-id :refer :all]))
+
+(deftest address->ocd-ids-test
+  (testing "empty input results in entire U.S."
+    (is (=
+           ["ocd-division/country:us"]
+           (address->ocd-ids {}))))
+  (testing "blank input results entire U.S."
+    (is (=
+           ["ocd-division/country:us"]
+           (address->ocd-ids {:street ""
+                              :street-2 ""
+                              :city ""
+                              :state ""
+                              :zip ""}))))
+  (testing "alabama state results in entire U.S. plus alabama"
+    (is (=
+           ["ocd-division/country:us"
+            "ocd-division/country:us/state:al"]
+           (address->ocd-ids {:street ""
+                              :street-2 ""
+                              :city ""
+                              :state "AL"
+                              :zip ""}))))
+  (testing "place works as well"
+    (is (=
+           ["ocd-division/country:us"
+            "ocd-division/country:us/state:al"
+            "ocd-division/country:us/state:al/place:birmingham"]
+           (address->ocd-ids {:street ""
+                              :street-2 ""
+                              :city "Birmingham"
+                              :state "AL"
+                              :zip ""})))))
+
+(deftest ocd-state-test
+  (testing "empty state -> nil"
+    (is (nil? (ocd-state "" "")))
+    (is (nil? (ocd-state "" nil))))
+  (testing "state builds valid ocd-id"
+    (is (=
+           (str default-ocd-id "/state:co")
+           (ocd-state default-ocd-id "CO")))))
+
+(deftest ocd-place-test
+  (testing "empty place -> nil"
+    (is (nil? (ocd-place "" "")))
+    (is (nil? (ocd-place "" nil))))
+  (testing "place builds valid ocd-id"
+    (is (=
+           "prefix/place:denver"
+           (ocd-place "prefix" "Denver")))))
